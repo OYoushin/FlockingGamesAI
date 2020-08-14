482acb1ba0b7f7f4eb3cc9526ceac79aaf6cdfa0
Tyrone Wang <s3607319@student.rmit.edu.au>
2020-08-14 13:16:35 +1000

    Initial Unity Project w/ Flocking implemented

---
+++ New Unity Project/Assets/Scripts/FlockAgent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockAgent : MonoBehaviour
+{
+    Flock agentFlock;
+    public Flock AgentFlock { get { return agentFlock;  } }
+
+    Collider2D agentCollider;
+
+    public Collider2D AgentCollider { get { return agentCollider; } }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agentCollider = GetComponent<Collider2D>();
+    }
+
+    public void Initialize(Flock flock)
+    {
+        agentFlock = flock;
+    }
+
+
+    public void Move(Vector2 velocity)
+    {
+        transform.up = velocity;
+        transform.position += (Vector3)velocity * Time.deltaTime;
+
+    }
+}
